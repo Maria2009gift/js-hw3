cf22c8a477a1655e85d787a4f3da8991fb9a6c8c
Maria2009gift <kovalenkom2009@gmail.com>
2023-06-16 19:10:53 +0300

    fix

--- main.js
+++ main.js
@@ -14,10 +14,10 @@ console.log(email.length)
 
 // -----3-----
 
-const str1 = "My "
-const str2 = "name "
+const str1 = "My"
+const str2 = "name"
 const str3 = "is"
-let fullName = str1 + str2 + str3
+let fullName = `${str1} ${str2} ${str3}`
 // fullName += "Viktor"
 console.log(`${fullName} Viktor`)
 
